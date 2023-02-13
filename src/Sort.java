99e27064b2a52803b845c3259664e61add6a661f
Zhejie Jiang <zhejiej@nycstudents.net>
2023-02-13 09:28:56 -0500

    finish

--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,78 @@
-package PACKAGE_NAME;public class Sort {
+import java.util.ArrayList;
+
+public class Sort
+{
+    // Part A.  Which Sort is Which?
+    public static void selectionSort(int[] elements)
+    {
+        int runtime = 0;
+        for (int j = 0; j < elements.length - 1; j++)
+        {
+            int minIndex = j;
+            for (int k = j + 1; k < elements.length; k++)
+            {
+                runtime++;
+                if (elements[k] < elements[minIndex])
+                {
+                    minIndex = k;
+                }
+            }
+            int temp = elements[j];
+            elements[j] = elements[minIndex];
+            elements[minIndex] = temp;
+        }
+        System.out.println(runtime);
+    }
+
+    public static void insertionSort(int[] elements)
+    {
+        int runtime = 0;
+        for (int j = 1; j < elements.length; j++)
+        {
+            int temp = elements[j];
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1])
+            {
+                runtime++;
+                elements[possibleIndex] = elements[possibleIndex - 1];
+                possibleIndex--;
+            }
+            elements[possibleIndex] = temp;
+        }
+        System.out.println(runtime);
+    }
+
+    // Part C.  Sorting a 1000-word list!
+    public static void selectionSortWordList(ArrayList<String> words)
+    {
+        for(int i = 0; i< words.size()-1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < words.size(); j++)
+            {
+                if ((words.get(j).compareTo( words.get(minIndex))) < 0 )
+                {
+                    minIndex = j;
+                }
+            }
+            String temp = words.get(i);
+            words.set(i,words.get(minIndex));
+            words.set(minIndex, temp);
+        }
+    }
+
+    public static void insertionSortWordList(ArrayList<String> words)
+    {
+        for (int i = 1; i < words.size(); i++)
+        {
+            String temp = words.get(i);
+            int possibleIndex = i;
+            while (possibleIndex > 0 && temp.compareTo(words.get(possibleIndex - 1)) < 0)
+            {
+                words.set(possibleIndex, words.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            words.set(possibleIndex, temp);
+        }
+    }
 }
--- src/WordSortTest.java
+++ src/WordSortTest.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class WordSortTest {
+import java.util.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class WordSortTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("------- TESTING SELECTION SORT -------");
+        ArrayList<String> allWords = new ArrayList<String>();
+
+        loadWordsInto(allWords);
+        System.out.println("This list contains " + allWords.size() + " common words");
+
+        System.out.println("Unsorted:");
+        System.out.println(allWords);
+
+        Sort.selectionSortWordList(allWords);
+
+        System.out.println("Sorted:");
+        System.out.println(allWords);
+
+        System.out.println("------- TESTING INSERTION SORT -------");
+        ArrayList<String> allWords2 = new ArrayList<String>();
+
+        loadWordsInto(allWords2);
+        System.out.println("This list contains " + allWords2.size() + " common words");
+
+        System.out.println("Unsorted:");
+        System.out.println(allWords2);
+
+        Sort.insertionSortWordList(allWords2);
+        System.out.println("Sorted:");
+        System.out.println(allWords2);
+    }
+
+    public static void loadWordsInto(ArrayList<String> wordList)
+    {
+        try
+        {
+            Scanner input = new Scanner(new File("words.txt"));
+            while (input.hasNext())
+            {
+                String word = input.next();
+                wordList.add(word);
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
 }
